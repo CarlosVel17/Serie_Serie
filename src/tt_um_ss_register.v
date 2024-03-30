cd679413d843889091b665f053314b0bbe422905
CarlosVel17 <165471233+CarlosVel17@users.noreply.github.com>
2024-03-30 17:27:45 -0600

    Update tt_um_ss_register.v

--- src/tt_um_ss_register.v
+++ src/tt_um_ss_register.v
@@ -5,7 +5,7 @@
 
 `define default_netname none
 
-module tt_um_ss_register (
+module tt_um_ss_register(
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -18,8 +18,8 @@ module tt_um_ss_register (
 
   // All output pins must be assigned. If not used, assign to 0.
     serie_serie_register U1 (.data_in(ui_in[0]), .clk(clk), .rst(rst_n), .ena(ena), .leri(ui_in[1]), .data_out(uo_out[0]));
-    assign uio_oe[7:0] = 8'b00000000;
-    assign uio_out[7:0] = 8'b00000000;
+    assign uio_oe = 0;
+    assign uio_out = 0;
     assign uo_out[7:1] = 7'b0000000;
 
 endmodule
